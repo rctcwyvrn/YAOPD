779d3decd867269d2d4f0377e8b2d197ab87d416
Howard <howard.lin314@gmail.com>
2020-02-16 13:16:58 -0500

    Switch from stupid handrolled bootleg threading to pools + call Invoke-Obfuscation scripts directly

--- generator/generator.py
+++ generator/generator.py
@@ -18,7 +18,7 @@ def get_random_dest():
 		domain = "http://" + hostname + ext + path
 		return domain
 	else:
-		ip = str(random.randint(0,200)) + "." + str(random.randint(0,200)) + "." + str(random.randint(0,200))+ "." + str(random.randint(0,200))
+		ip = "http://" + str(random.randint(0,200)) + "." + str(random.randint(0,200)) + "." + str(random.randint(0,200))+ "." + str(random.randint(0,200))
 		return ip + path
 
 def get_random_text(min, max):
@@ -59,8 +59,8 @@ def inject_shellcode_cmd():
 # commands = [remote_payload_cmd, schtasks_persistence_cmd, set_registry_key_cmd, inject_shellcode_cmd]
 commands = [remote_payload_cmd, schtasks_persistence_cmd, set_registry_key_cmd] # inject shellcode seems to be broken when we try to obfuscate it :c
 
-# AST doesnt work with schtasks and remote_payload
-# set registry key has a 100% fail rate
+# Testing:
+#commands = [remote_payload_cmd]
 
 def generate_script():
 	script = ""
@@ -77,163 +77,90 @@ def generate_raw(n):
 		f.write(script)
 		f.close()
 
-#invoke_obfuscation_choices = ["TOKEN,ALL,1,HOME","AST,ALL,1,HOME","STRING,ALL,1,HOME","ENCODING,ALL,1,HOME"]
-invoke_obfuscation_choices = ["TOKEN,ALL,1,HOME","STRING,ALL,1,HOME"]
-invoke_obfuscation_encodings = [f",ENCODING,{str(i+1)},1,HOME" for i in range(8)]
-# print(invoke_obfuscation_choices)
+invoke_obfuscation_choices = ["Out-ObfuscatedTokenCommand","Out-ObfuscatedStringCommand", "Out-SecureStringCommand -PassThru"] #"Out-ObfuscatedAST" doesnt work for some reason
 
-def obfuscate(script_num, conn):
+invoke_obfuscation_encodings = ["Out-EncodedAsciiCommand", "Out-EncodedBXORCommand", "Out-EncodedBinaryCommand", "Out-EncodedHexCommand", "Out-EncodedOctalCommand", "Out-EncodedSpecialCharOnlyCommand", "Out-EncodedWhitespaceCommand"]
 
-	package = [[],[],[],[]]
-	try:
-		#obfs_commands = ",".join([random.choice(invoke_obfuscation_choices) for _ in range(random.randint(1,4))])
-		obfs_commands = random.choice(invoke_obfuscation_choices)
 
-		if(random.randint(0,1) == 0):
-			obfs_commands += random.choice(invoke_obfuscation_encodings)
+# Testing
+#invoke_obfuscation_choices = ["Out-ObfuscatedStringCommand"]
+#invoke_obfuscation_encodings = ["Out-EncodedAsciiCommand"]
 
-		if(random.randint(0,1) == 0):
-			obfs_commands+=",COMPRESS,1,1,HOME"
+def obfuscate(script_num):
+	failed = False 
+	obfs_choice = random.choice(invoke_obfuscation_choices)
 
-		obfs_commands+= ",OUT"
+	#print(f"> Obfuscating script #{script_num}, commands for Invoke-Obfuscation = {obfs_choice}")
 
-		print(f"> Obfuscating script #{script_num}, commands for Invoke-Obfuscation = {obfs_commands}")
+	p = pexpect.spawn(f"pwsh -Command \"Import-Module ./Invoke-Obfuscation/Invoke-Obfuscation.psd1; {obfs_choice} -Path ./res/dataset-{str(script_num)}-raw.ps1\" > ./res/dataset-{str(script_num)}-obfs.ps1 2>> ./res/err.log")
+	p.wait()
 
-		p = pexpect.spawn(f"pwsh obfuscate.ps1 -Filename ./res/dataset-{str(script_num)}-raw.ps1 -Command {obfs_commands}")
-		#p.read_nonblocking(size=10000)
-		p.expect_exact("Enter path for output file (or leave blank for default): ")
-		p.sendline(f"./res/dataset-{str(script_num)}-obfs.ps1")
-		p.wait() #this is the only one that works multithreaded
-		#p.interact() #this works but only singlethreaded
-		#p.expect(pexpect.EOF) this should work but it just doesnt
-		print(f"> Script #{script_num} obfuscation succeeded!")
-		p.close()
+	# Encode sometimes
+	# NOTE: Encoding is very inconsistent (maybe 25% success rate?) so let's just encode everything and pretend we "chose" not to encode the ones that failed
+	encoded = False
+	if(random.randint(0,10) > -1):
+		encoding_choice = random.choice(invoke_obfuscation_encodings) + " -PassThru"
+		encoded = True
 
-		package[0].append(str(script_num))
-		package[1].append(obfs_commands)
+		#print(f"> Choosing to encode script #{script_num}, encoding = {obfs_choice}")
+		#os.system(f"cat ./res/dataset-{str(script_num)}-obfs.ps1 > ./res/sanity_{str(script_num)}.txt")
 
-	except Exception as e:
-		print(f"> !!! Obfuscation for script #{script_num} failed! Command = {obfs_commands}")
-		print(e)
+		with open(f"./res/dataset-{str(script_num)}-obfs.ps1") as f:
+			script = "".join(f.readlines())
+			#print(f"> midpoint script = {script}")
+			script = "{" + script + "}"
+			#p = pexpect.spawn(f"pwsh -Command \"Import-Module ./Invoke-Obfuscation/Invoke-Obfuscation.psd1; {encoding_choice} -Path ./res/dataset-{str(script_num)}-obfs.ps1\" > ./res/dataset-{str(script_num)}-obfs.ps1")
+			p = pexpect.spawn(f"pwsh -Command \"Import-Module ./Invoke-Obfuscation/Invoke-Obfuscation.psd1; {encoding_choice} -Script {script}\" > ./res/dataset-{str(script_num)}-obfs-enc.ps1 2>> ./res/err.log")
+			p.wait()
 
-		package[2].append(str(script_num))
-		package[3].append(obfs_commands)
+			try:
+				f = open(f"./res/dataset-{str(script_num)}-obfs-enc.ps1")
+				f.close()
+				os.system(f"rm ./res/dataset-{str(script_num)}-obfs.ps1; mv ./res/dataset-{str(script_num)}-obfs-enc.ps1 ./res/dataset-{str(script_num)}-obfs.ps1")
+			except Exception as e:
+				#failed = True
+				encoded = False #Just pretend the encoding never happened lul
 
-	conn.send(package)
-	conn.close()
+	# Compress sometimes, WIP
+	compressed = False
+	# if (random.randint(0,1) == 1):
+	# 	compressed = True
+	# 	print(f"> Choosing to compress script #{script_num}")
+	# 	p = pexpect.spawn(f"pwsh -Command \"Import-Module ./Invoke-Obfuscation/Invoke-Obfuscation.psd1; Out-CompressedCommand -NoProfile -NonInteractive -NoLogo -Path ./res/dataset-{str(script_num)}-obfs.ps1\" > ./res/dataset-{str(script_num)}-obfs.ps1")
+	# 	p.wait()
+
+	# Test for success
+	with open(f"./res/dataset-{str(script_num)}-obfs.ps1") as f:
+		lines = f.readlines()
+		#print("> Generated lines = " + "\n".join(lines))
+
+		log = f"echo {str(script_num)}\t{obfs_choice}"
+		if encoded:
+			log += "\t" + encoding_choice
+		else:
+			log += "\tnot_encoded"
+
+		if compressed:
+			log += "\tcompressed"
+		else:
+			log += "\tnot_compressed"
+
+		if len(lines) == 0 or failed:
+			print(f"> !!! Script #{script_num} obfuscation failed!!")
+			os.system(f"rm ./res/dataset-{str(script_num)}-obfs.ps1 ./res/dataset-{str(script_num)}-raw.ps1")
+
+			log += " >> ./res/failed.log"
+
+		else:
+			log += " >> ./res/success.log"
+			print(f"> Script #{script_num} obfuscation succeeded!")
+
+		os.system(log)
+	p.close()
 	return
 
 
 THREAD_NUM = int(sys.argv[2])
-POLL_CAP = 50
-
-success = []
-failed = []
-succ_cmds = []
-failed_cmds = []
-
-def run_threads(targets):
-	print("--- Starting to run threads")
-	global success, failed, succ_cmds, failed_cmds
-	pipes = []
-	thread_queue = queue.Queue()
-	polling_queue = queue.Queue()
-
-	for target in targets:
-		parent, child = multiprocessing.Pipe()
-		t = multiprocessing.Process(target=obfuscate, args=[target, child])
-		thread_queue.put([t,parent,target])
-
-	for _ in range(THREAD_NUM):
-		t,parent,target = thread_queue.get()
-		t.start()
-		polling_queue.put([parent,target,t,0])
-		#time.sleep(3)
-		time.sleep(int(70/THREAD_NUM)) #sleep just enough so that the first thread will be done when the last of the first group has gone
-
-	print("--- Started first batch")
-	while(not thread_queue.empty()):
-		t,parent,target = thread_queue.get()
-		found = False
-
-		#print(f"--- Starting polling to find a spot for thread target={target}")
-		while not found and not polling_queue.empty():
-
-			poll_pipe, poll_target, poll_thread, count = polling_queue.get()
-
-			poll_time = 3/(polling_queue.qsize()+1) #because we want to be more relaxed the fewer polling targets we have, or else we always 'timeout' on the last few
-			try:
-				if(poll_pipe.poll(poll_time)):
-				#if(not poll_thread.is_alive()):
-					package = poll_pipe.recv()
-					print(f"--- Received package from script #{poll_target} after {count} tries")
-					success += package[0]
-					succ_cmds += package[1]
-					failed += package[2]
-					failed_cmds += package[3]
-
-					poll_thread.join()
-					found = True
-				else:
-					if(count>POLL_CAP):
-						print(f"--- Spent over {POLL_CAP} tries polling for result from script # {poll_target}, assuming the thread is dead")
-						failed.append(poll_target)
-						failed_cmds.append("TIMEOUT")
-						poll_thread.terminate()
-						found = True #since we terminated a thread, we want to start another one
-
-					else:
-						polling_queue.put([poll_pipe, poll_target, poll_thread, count+1])
-						#time.sleep(1)
-
-			except Exception as e:
-				print(f"!!! Caught exception while trying to receive package from {poll_target}")
-				print(e)
-				failed.append(poll_target)
-				failed_cmds.append("EXCEPTION")
-
-
-		t.start()
-		polling_queue.put([parent,target,t,0])
-
-	print("--- All threads have been started, waiting for them to finish")
-	#FIXME: abstract into a function please
-	while not polling_queue.empty():
-
-			poll_pipe, poll_target, poll_thread, count = polling_queue.get()
-
-			poll_time = 3/(polling_queue.qsize()+1)
-			try:
-				if(poll_pipe.poll(poll_time)):
-				#if(not poll_thread.is_alive()):
-					package = poll_pipe.recv()
-					print(f"--- Received package from script #{poll_target}")
-					success += package[0]
-					succ_cmds += package[1]
-					failed += package[2]
-					failed_cmds += package[3]
-
-					poll_thread.join()
-					found = True
-				else:
-					if(count>POLL_CAP):
-						print(f"--- Spent over {POLL_CAP} tries polling for result from script # {poll_target}, assuming the thread is dead")
-						failed.append(poll_target)
-						failed_cmds.append("TIMEOUT")
-						poll_thread.terminate()
-
-					else:
-						polling_queue.put([poll_pipe, poll_target, poll_thread, count+1])
-						#time.sleep(1)
-
-			except Exception as e:
-				print(f"!!! Caught exception while trying to receive package from {poll_target}")
-				print(e)
-				failed.append(poll_target)
-				failed_cmds.append("EXCEPTION")
-
-	print("--- Done!")
-	return success, failed, succ_cmds, failed_cmds
 
 def generate_obfs(n):
 	
@@ -242,16 +169,8 @@ def generate_obfs(n):
 
 	print("Converting to obfuscated powershell")
 
-	run_threads(range(n))
-
-	print(f"Succeeded {str(len(success))}/{str(n)}")
-
-	for fail in failed:
-		print(f"Deleting failed script {fail}")
-		os.system(f"rm res/dataset-{str(fail)}-raw.ps1")
-
-	#print("succeded", succ_cmds)
-	#print("failed", failed_cmds)
-	print("Failed #", failed)
+	#run_threads(range(n))
+	with multiprocessing.Pool(THREAD_NUM) as p:
+		p.map(obfuscate, range(n))
 
 generate_obfs(int(sys.argv[1]))
